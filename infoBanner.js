940fcc18dd0f3480a1e4d5108e0bb7e789b9f7bd
trupti982 <113361482+trupti982@users.noreply.github.com>
2023-03-16 19:46:19 +0530

    Create infoBanner.js

---
+++ infoBanner.js
@@ -0,0 +1,111 @@
+AFRAME.registerComponent("info-banner", {
+  schema: {
+    itemId: { default: "", type: "string" },
+  },
+  update: function () {
+    this.createBanner();
+  },
+  createBanner: function () {
+    postersInfo = {
+      superman: {
+        banner_url: "./assets/posters/marvelbanner.png",
+        title: "Marvel Comics",
+        released_year: "1939",
+        description:
+          "Marvel Comics is the brand name and primary imprint of Marvel Worldwide Inc., formerly Marvel Publishing, Inc. and Marvel Comics Group, a publisher of American comic books and related media. In 2009, The Walt Disney Company acquired Marvel Entertainment, Marvel Worldwide's parent company.",
+      },
+      spiderman: {
+        banner_url: "./assets/posters/tinklebanner.png",
+        title: "Tinkle",
+        released_year: "1980",
+        description:
+          "Tinkle is an Indian fortnightly magazine for children, published mainly in India. Originally owned by the India Book House, the Tinkle brand was acquired by ACK Media in 2007.",
+      },
+      "captain-aero": {
+        banner_url: "./assets/posters/avengersbanner.png",
+        title: "Avengers",
+        released_year: "2012",
+        description:
+          "The Avengers are a fictional team of superheroes appearing in American comic books published by Marvel Comics. The team made its debut in The Avengers #1, created by writer-editor Stan Lee and artist/co-plotter Jack Kirby.",
+      },
+      "outer-space": {
+        banner_url: "./assets/posters/dcbanner.png",
+        title: "Detective Comics",
+        released_year: "1937",
+        description:
+          "DC Comics, Inc. is an American comic book publisher and the flagship unit of DC Entertainment, a subsidiary of the Warner Bros. Global Brands and Experiences division of Warner Bros., which itself is a subsidiary of AT&T's WarnerMedia through its Studios & Networks division. ",
+      },
+    };
+    const { itemId } = this.data;
+
+    const fadeBackgroundEl = document.querySelector("#fadeBackground");
+
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("id", `${itemId}-banner`);
+
+    entityEl.setAttribute("geometry", {
+      primitive: "plane",
+      width: 0.9,
+      height: 1,
+    });
+
+    entityEl.setAttribute("material", { color: "#000" });
+    entityEl.setAttribute("position", { x: 0, y: 0.1, z: -1 });
+
+    const item = postersInfo[itemId];
+
+    const imageEl = this.createImageEl(item);
+    const titleEl = this.createTitleEl(item);
+    const descriptionEl = this.createDescriptionEl(item);
+
+    entityEl.appendChild(imageEl);
+    entityEl.appendChild(titleEl);
+    entityEl.appendChild(descriptionEl);
+
+    fadeBackgroundEl.appendChild(entityEl);
+  },
+  createImageEl: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "plane",
+      width: 0.85,
+      height: 0.4,
+    });
+    entityEl.setAttribute("material", { src: item.banner_url });
+    entityEl.setAttribute("position", { x: 0, y: 0.3, z: 0.05 });
+    return entityEl;
+  },
+  createTitleEl: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("text", {
+      shader: "msdf",
+      anchor: "left",
+      font: "https://cdn.aframe.io/examples/ui/Viga-Regular.json",
+      width: 1.2,
+      height: 2,
+      color: "#fff",
+      value: `${item.title} (${item.released_year})`,
+    });
+    entityEl.setAttribute("position", { x: -0.4, y: 0.02, z: 0.05 });
+    return entityEl;
+  },
+  createDescriptionEl: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("text", {
+      shader: "msdf",
+      anchor: "left",
+      font: "https://cdn.aframe.io/examples/ui/Viga-Regular.json",
+      width: 0.75,
+      height: 2,
+      color: "#fff",
+      wrapCount: "40",
+      value: item.description,
+    });
+    entityEl.setAttribute("position", { x: -0.4, y: -0.24, z: 0.05 });
+    return entityEl;
+  },
+});
